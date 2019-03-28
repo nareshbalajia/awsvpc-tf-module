27211a92d320f3df50bdca777b9ddb4ded74143f
Naresh <na@logitech.com>
2019-03-28 11:21:05 +0530

    fixing terraform syntax issues

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 # TF output resources
 output "vpc_id" {
-  value = "${aws_vpc.main.id}"
+  value = "${aws_vpc.primary_vpc.id}"
 }
 
 output "vpc_cidr_block" {
-  value = "${aws_vpc.main.cidr_block}"
+  value = "${aws_vpc.primary_vpc.cidr_block}"
 }
 
 output "private_subnets_ids" {
@@ -12,5 +12,5 @@ output "private_subnets_ids" {
 }
 
 output "private_route_table_ids" {
-  value = ["${aws_route_table.private_subnets.*.id}"]
+  value = ["${aws_route_table.public_rt.*.id}"]
 }
