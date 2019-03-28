b8b18a7660ca80b5443d5c7c7018b18679a14e0d
Naresh <na@logitech.com>
2019-03-28 11:09:59 +0530

    removing interpolation for backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "${var.state_bucket}"
     key = "terraform/backend/vpc-infra/vpc_infra/terraform.tfstate"
-    region = "${var.region}"
     encrypt = true
   }
 }
